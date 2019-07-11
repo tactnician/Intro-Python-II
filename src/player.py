4522f5a1f498b579314db2583195aa503c85f3c1
tactnician <45316614+tactnician@users.noreply.github.com>
2019-07-11 13:34:39 -0400

    Revert " mvp"

--- src/player.py
+++ src/player.py
@@ -1,12 +1,2 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-
-class Player:
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
-    
-    def make_move(self, new_location):
-        self.location = new_location
-        print(f'You are in the {self.location.name}')
-        print(self.location.description)
